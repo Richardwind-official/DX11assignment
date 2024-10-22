bfd1bdd37598d6981d5ba174d12e572ffacec16d
Richard_FC <151904244+Richardwind-official@users.noreply.github.com>
2024-10-22 14:40:04 +0800

    test
    hello world

---
+++ Helloworld.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// using ll = long long;
+#define int long long
+const int N = 1e5 + 3;
+const int inf = 1e9 + 3;
+
+void solve(){
+	cout << "Hello World" << endl;
+}
+
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); 
+	int T; 
+	// cin >> T;
+	// while(T--)
+	solve();
+	return 0;
+}
